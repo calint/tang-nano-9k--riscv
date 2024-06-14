fd5d2df930cf3224d130eb220216a63158a751e8
calint <calin.tenitchi@gmail.com>
2024-06-14 22:52:09 +0200

    .

--- configuration.py
+++ configuration.py
@@ -1,3 +1,7 @@
+#
+# if file changed run `configuration-generate.py`
+# if `RAM_ADDR_WIDTH` is modified recompile `os`
+#
 RAM_FILE = "os/os.mem" # path to initial RAM content
 RAM_ADDR_WIDTH = 13    # 2^13*4 (32768) bytes of RAM
 UART_BAUD_RATE = 9600  # 9600 baud, 8 bits, 1 stop bit, no parity
